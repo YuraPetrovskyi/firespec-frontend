916773b02976b2ddeb3975b0cdc76ecad4b024f3
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-04-30 11:58:29 +0100

    feat: make ToasteProvider for ensure that layout works as a server component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Toaster } from 'react-hot-toast';
+import ToasterProvider from "@/components/ToasterProvider";
 
 export const metadata: Metadata = {
   title: "FireSpec",
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-          <Toaster position="top-right" reverseOrder={false} />
+        <ToasterProvider />
           {children}
       </body>
     </html>
