e3bfc11394669144aff1fca5c0b06d956970f9a5
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-05-04 19:09:10 +0100

    style: add some styles and Cancel button

--- src/components/CreateProjectModal.tsx
+++ src/components/CreateProjectModal.tsx
@@ -41,13 +41,8 @@ export default function CreateProjectModal({ isOpen, onClose, onProjectCreated }
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white p-8 rounded shadow-md w-96 relative">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-xl"
-        >
-          ×
-        </button>
-        <h2 className="text-2xl font-semibold mb-6">➕ Create New Project</h2>
+        
+        <h2 className="text-2xl font-semibold mb-6 text-center"> Create New Project</h2>
 
         <div className="flex flex-col gap-4">
           <input
@@ -64,12 +59,20 @@ export default function CreateProjectModal({ isOpen, onClose, onProjectCreated }
             onChange={(e) => setNewClient(e.target.value)}
             className="border p-2 rounded"
           />
-          <button
-            onClick={handleCreateProject}
-            className="bg-green-600 text-white py-2 rounded hover:bg-green-700 transition"
-          >
-            ✅ Save Project
-          </button>
+          <div className='flex justify-between items-center mt-4 font-semibold'>
+            <button
+              onClick={onClose}
+              className="bg-gray-300 hover:bg-gray-400 text-gray-800 px-4 py-2 rounded transition"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={handleCreateProject}
+              className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition"
+            >
+              Save Project
+            </button>
+          </div>
         </div>
       </div>
     </div>
