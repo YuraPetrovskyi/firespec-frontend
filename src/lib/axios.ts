72989153e4a03addba3b15652157681872f0a0cb
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-05-09 12:19:45 +0100

    build: make custum axios for taken token

---
+++ src/lib/axios.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // http://127.0.0.1:8000/api
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// ⏳ автоматично додає токен перед кожним запитом
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // const token = localStorage.getItem('token');
+    const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// ⛔️ автоматична обробка 401
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      window.location.href = '/'; // або можна використати router.push('/')
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
