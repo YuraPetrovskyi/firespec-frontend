d381f3c1a9db8db23152237ae951462ed636abad
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-05-30 20:04:42 +0100

    feat: add new style to tailwind config for animating modals

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0', transform: 'scale(0.95)' },
+          '100%': { opacity: '1', transform: 'scale(1)' },
+        },
+        'zoom-in': {
+          '0%': { opacity: '0', transform: 'scale(0.5)' },
+          '100%': { opacity: '1', transform: 'scale(1)' },
+        },
+        'zoom-out': {
+          '0%': { opacity: '1', transform: 'scale(1)' },
+          '100%': { opacity: '0', transform: 'scale(0.5)' },
+        },
+        'slide-down': {
+          '0%': { opacity: '0', transform: 'translateY(-20%) scale(0.95)' },
+          '100%': { opacity: '1', transform: 'translateY(0) scale(1)' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-out forwards',
+        'zoom-in': 'zoom-in 0.5s ease-out forwards',
+        'zoom-out': 'zoom-out 0.5s ease-in forwards',
+        'slide-down': 'slide-down 0.3s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 };
