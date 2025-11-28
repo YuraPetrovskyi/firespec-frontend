cd8994f0ee15481b81b2f61b66a89ff190e5e2db
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-11-28 17:15:22 +0000

    fix: hide registration form

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -21,7 +21,9 @@ export default function LoginModal() {
     e.preventDefault();
     try {
       const endpoint = isRegistering ? "register" : "login";
-      const payload = isRegistering ? { name, email, password } : { email, password };
+      const payload = isRegistering
+        ? { name, email, password }
+        : { email, password };
       // console.log("payload", payload);
       // console.log("endpoint", endpoint);
 
@@ -36,93 +38,116 @@ export default function LoginModal() {
 
   return (
     <div className="fixed inset-0 z-50 bg-black bg-opacity-40 backdrop-blur-sm flex items-center justify-center">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-red-100 rounded-lg shadow-lg w-96 max-w-md p-8 "
-      >
-        <p className="text-2xl text-red-500 text-center font-bold my-6">Welcome to FireSpec</p>
+      <div className="bg-red-100 rounded-lg shadow-lg max-w-md w-full p-8 min-w-[300px]">
+        <p className="text-2xl text-red-500 text-center font-bold my-6">
+          Welcome to FireSpec
+        </p>
         <p className="text-xl text-center font-bold mb-4">
           {isRegistering ? "Register Your Account" : "Sign Into Your Account"}
         </p>
-        {error && <p className="text-red-600 text-2xl mb-2">{error}</p>}
-        {isRegistering && (
+        <form onSubmit={handleSubmit}>
+          {error && <p className="text-red-600 text-2xl mb-2">{error}</p>}
+          {isRegistering && (
+            <input
+              className="w-full border px-3 py-2 mb-3 rounded-xl"
+              type="text"
+              placeholder="Enter Full Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          )}
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Email
+          </label>
           <input
             className="w-full border px-3 py-2 mb-3 rounded-xl"
-            type="text"
-            placeholder="Enter Full Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            type="email"
+            placeholder="Enter Your Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
-        )}
-        <input
-          className="w-full border px-3 py-2 mb-3 rounded-xl"
-          type="email"
-          placeholder="Enter Your Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        {/* <input
-          className="w-full border px-3 py-2 mb-3 rounded-xl"
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        /> */}
-        <div className="relative w-full">
-          <input
+          {/* <input
             className="w-full border px-3 py-2 mb-3 rounded-xl"
-            type={showPassword ? "text" : "password"} // Перемикання між "password" і "text"
+            type="password"
             placeholder="Enter Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-          />
-          <button
-            type="button"
-            className="absolute right-3 top-2 text-gray-600"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword 
-              ? <Image src="eye.svg" alt="Logo" width={20} height={20} className="cursor-pointer h-auto" priority/>
-              : <Image src="eye-off.svg" alt="Logo" width={20} height={20} className="cursor-pointer h-auto" priority/>}
-          </button>
-        </div>
-        <button
-          type="submit"
-          className="bg-slate-500 text-white w-full py-2 rounded-xl
-            hover:bg-slate-700 hover:scale-105 active:scale-95 transition duration-200"
-        >
-          {isRegistering ? "Register" : "Login"}
-        </button>
-
-        {!isRegistering && (
-          <div className="text-center mt-3">
-            <Link 
-              href="/forgot-password" 
-              className="text-sm text-blue-600 hover:underline"
+          /> */}
+          <div className="relative w-full">
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              Password
+            </label>
+            <input
+              className="w-full border px-3 py-2 mb-3 rounded-xl"
+              type={showPassword ? "text" : "password"} // Перемикання між "password" і "text"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button
+              type="button"
+              className="absolute right-3 top-10 text-gray-600"
+              onClick={() => setShowPassword(!showPassword)}
             >
-              Forgot password?
-            </Link>
+              {showPassword ? (
+                <Image
+                  src="eye.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                  className="cursor-pointer h-auto"
+                  priority
+                />
+              ) : (
+                <Image
+                  src="eye-off.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                  className="cursor-pointer h-auto"
+                  priority
+                />
+              )}
+            </button>
           </div>
-        )}
+          <button
+            type="submit"
+            className="bg-slate-500 text-white w-full py-2 rounded-xl
+              hover:bg-slate-700 hover:scale-105 active:scale-95 transition duration-200"
+          >
+            {isRegistering ? "Register" : "Login"}
+          </button>
 
-        <button
-          type="button"
-          className="text-sm text-center w-full mt-8 text-cyan-600 my-4
-            hover:scale-105 active:scale-95 transition duration-200"
-          onClick={() => {
-            setError("");
-            setIsRegistering(!isRegistering);
-          }}
-        >
-          {isRegistering
-            ? "Already have an account? Login Here"
-            : "Don't have an account? Register Here"}
-        </button>
-      </form>
+          {!isRegistering && (
+            <div className="text-center mt-3">
+              <Link
+                href="/forgot-password"
+                className="text-sm text-blue-600 hover:underline"
+              >
+                Forgot password?
+              </Link>
+            </div>
+          )}
+
+          {/* <button
+            type="button"
+            className="text-sm text-center w-full mt-8 text-cyan-600 my-4
+              hover:scale-105 active:scale-95 transition duration-200"
+            onClick={() => {
+              setError("");
+              setIsRegistering(!isRegistering);
+            }}
+          >
+            {isRegistering
+              ? "Already have an account? Login Here"
+              : "Don't have an account? Register Here"}
+          </button> */}
+        </form>
+      </div>
     </div>
   );
 }
